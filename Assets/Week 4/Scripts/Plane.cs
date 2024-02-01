5f8f978808bdd0c6276b12fdec582fcc94502ee2
Osama Hussein <osamahusseinn2@gmail.com>
2024-02-01 14:25:15 -0500

    I completed Task 2 where I got the plane updating its flightpath!

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Plane : MonoBehaviour
@@ -9,6 +10,10 @@ public class Plane : MonoBehaviour
     public float pointThreshold = 0.2f;
 
     LineRenderer lineRenderer;
+    Rigidbody2D rigidbody;
+    Vector2 currentPosition;
+
+    public float speed = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +21,42 @@ public class Plane : MonoBehaviour
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
+
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        currentPosition = transform.position;
+
+        if(points.Count > 0)
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rigidbody.rotation = -angle;
+        }
+
+        rigidbody.MovePosition(rigidbody.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+
+        if (points.Count > 0)
+        {
+            if(Vector2.Distance(currentPosition, points[0]) < pointThreshold)
+            {
+                points.RemoveAt(0);
+
+                for(int i = 0; i < lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                }
+
+                lineRenderer.positionCount--;
+            }
+        }
     }
 
     private void OnMouseDown()
